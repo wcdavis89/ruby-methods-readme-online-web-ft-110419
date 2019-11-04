0502b6463b70128b523433183c4cd4c9fb4c6898
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 23:09:07 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,15 +1,3 @@
-# Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
 puts phrase
 
 def greeting
