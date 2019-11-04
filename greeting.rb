3064adbeb7ff8e83f67b4a59517f81175662d5d8
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 23:07:24 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,21 @@
+# Use this space to code along with the readme
+
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+def greeting
+  puts "Hello World"
+end
+
+5.times do
+  puts greeting
+end
